f445ea99c7a4d95003f795c022e286809b4a6cd2
Daniel Moschnitzka <d.moschnitzka@gmail.com>
2022-11-05 17:23:45 +0100

    game start added

--- game.js
+++ game.js
@@ -1,17 +1,18 @@
 let buttonColors = ["red", "blue", "green", "yellow"];
 
+let gameRunning = "false";
+let level = 0;
+
 let gamePattern = [];
 let userClickedPattern = []
 
-$(".btn").on("click", function() { //addEventListener to all .btn classes
-  let userChosenColor = $(this).attr("id");
-  userClickedPattern.push(userChosenColor);
-  playSound(userChosenColor);
+$(document).keydown(function(event) {
+	if(event.key === "Enter" && gameRunning === "false") {
+		gameRunning = "true";
+		$("#level-title").text("Level " + level);
+		nextSequence();
+	}
 });
-
-// function clickHandler() {
-//   console.log("yes");
-// }
 
 function nextSequence() {
   let randomNumber = Math.floor(Math.random() * 4);
@@ -21,9 +22,27 @@ function nextSequence() {
   //make button flash and play audio
   $("#" + randomChosenColor).fadeOut(100).fadeIn(100);
   playSound(randomChosenColor);
+	$("#level-title").text("Level " + level);
+	level += 1;
 }
 
+$(".btn").on("click", function() { //addEventListener to all .btn classes
+  let userChosenColor = $(this).attr("id");
+  userClickedPattern.push(userChosenColor);
+  playSound(userChosenColor);
+	animatePress(userChosenColor);
+});
+
+//creates sound object for the pressed color and plays it
 function playSound(color) {
   let audio = new Audio("sounds/" + color + ".mp3");
   audio.play();
+}
+
+//animates pressed button
+function animatePress(currentColor) {
+	$("." + currentColor).addClass("pressed");
+	setTimeout(function() {
+		$("." + currentColor).removeClass("pressed");
+	}, 120);
 }

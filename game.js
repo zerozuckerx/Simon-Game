77cd6e95207bd87cc59a311c406b15b447629338
Daniel Moschnitzka <d.moschnitzka@gmail.com>
2022-11-06 14:38:28 +0100

    game functionality

--- game.js
+++ game.js
@@ -1,46 +1,64 @@
 let buttonColors = ["red", "blue", "green", "yellow"];
 
-let gameRunning = "false";
-let level = 0;
+let gameRunning = false;
+let level = 1;
 
 let gamePattern = [];
 let userClickedPattern = []
 
 $(document).keydown(function(event) {
-	if(event.key === "Enter" && gameRunning === "false") {
-		gameRunning = "true";
-		$("#level-title").text("Level " + level);
+
+	if(!gameRunning && event.key === "Enter") {
+		// !gameRunning is equal to gameRunning === false
+
+		// $("#level-title").text("Level " + level);
 		nextSequence();
+		gameRunning = true;
 	}
 });
 
 function nextSequence() {
+
+	$("#level-title").text("Level " + level);
+
+	level++;
+
   let randomNumber = Math.floor(Math.random() * 4);
   let randomChosenColor = buttonColors[randomNumber];
   gamePattern.push(randomChosenColor);
 
-  //make button flash and play audio
+  // make button flash and play audio
   $("#" + randomChosenColor).fadeOut(100).fadeIn(100);
   playSound(randomChosenColor);
-	$("#level-title").text("Level " + level);
-	level += 1;
+;
 }
 
-$(".btn").on("click", function() { //addEventListener to all .btn classes
+function checkAnswer(currentLevel) {
+
+}
+	// }
+
+
+// addEventListener to all .btn classes
+$(".btn").on("click", function() {
+
   let userChosenColor = $(this).attr("id");
   userClickedPattern.push(userChosenColor);
+
   playSound(userChosenColor);
 	animatePress(userChosenColor);
+	checkAnswer(userChosenColor);
 });
 
-//creates sound object for the pressed color and plays it
+// creates sound object for the pressed color and plays it
 function playSound(color) {
   let audio = new Audio("sounds/" + color + ".mp3");
   audio.play();
 }
 
-//animates pressed button
+// animates pressed button
 function animatePress(currentColor) {
+
 	$("." + currentColor).addClass("pressed");
 	setTimeout(function() {
 		$("." + currentColor).removeClass("pressed");
